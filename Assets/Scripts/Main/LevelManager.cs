81e270dc82165adeaf1da732707d92b8d6ced6f8
Mrn <mirren91@hotmail.com>
2020-05-05 10:23:28 +0100

    Safety backup before PC reset

---
+++ Assets/Scripts/Main/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static LevelManager levelManager;
+    private string _currentLoadedLevel = "";
+
+    private void Awake()
+    {
+        if (levelManager != null)
+        {
+            GameObject.Destroy(levelManager);
+        }
+        else
+        {
+            levelManager = this;
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
